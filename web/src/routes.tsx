4663998623ade5ccdd143c2540c2fd88d99c3155
Thiago Peres <thiago.mourao.peres@gmail.com>
2020-08-05 03:39:25 -0300

    Create PageHeader/Teacher List

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
+import TeacherList from './pages/TeacherList';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
+      <Route path="/study" component={TeacherList} />
     </BrowserRouter>
   )
 }
